a81907cfa3c4801972642fd209a109c6e8a47d2d
Muhammad Noman <muhammadnomanmle@gmail.com>
2025-02-14 12:04:49 +0500

    Update Linearsearch.cpp

--- Linearsearch.cpp
+++ Linearsearch.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int A[10] = {1,2,3,4,5,6,7,8,9,10};
-n = 10;
-cout<<"Enter Your Number: "<<endl;
-for(int i=0; i>n; i++) {
-    cin>>A[i];
+int main() {
+    int A[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+    int n = 10;
+    int Key;  
+    
+    cout << "Enter Your Numbers (to replace the initialized ones):" << endl;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    cout << "Enter Key: ";
+    cin >> Key;  
+
+    
+    for(int i = 0; i < n; i++) {
+        if(Key == A[i]) {
+            cout << "Found at index " << i << endl;
+            return 0; 
+        }
+    }
+
+    
+    cout << "Not found" << endl;
+    
+    return 0;
 }
-cout<<"Enter Key:" ;
-cin>>Key;
-for(i=0; i<n; i++)
-{
-     if(key==A[i])
-     {
-        cout<<"found at"<<i;
-     }
-     return 0;
-}
-  cout<<"Not found "<<endl;
